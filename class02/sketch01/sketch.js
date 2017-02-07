2e5e09b7da9a615e5b04c11ced59e273fa8fa16f
Winnie Soon <siusoon@gmail.com>
2017-02-07 19:05:04 +0100

    Update sketch.js

--- class02/sketch01/sketch.js
+++ class02/sketch01/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-
+ console.log("Hello World");
+ createCanvas(800, 600);   //create a drawing canvas
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  fill(100);  //check this syntax with alpha value
+  rect(0, 0, width, height);
+  ellipse(400, 300, 80, 80);  //draw an ellipse
 }
